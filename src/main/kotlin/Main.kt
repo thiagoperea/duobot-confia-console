35a71d07dee307c29d6e165ed1f5b6a712f93eb2
Thiago Perea <thiago.perea1@gmail.com>
2022-06-14 17:23:37 -0300

    one algorithm to rule them all

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,17 +9,27 @@ fun main() = runBlocking {
     // download all the game versions available. This will return a List<String>
     val gameVersions = riotService.listVersions()
 
-    println("Game Versions available:")
-    println(gameVersions.toString())
+//    println("Game Versions available:")
+//    println(gameVersions.toString())
 
     // get the first position (or the newest version)
     val lastestVersion = gameVersions.first()
 
     // download all the champions from this version
-    val championList = riotService.getAllChampions(lastestVersion)
+    val championListResponse = riotService.getAllChampions(lastestVersion)
+    val championList = championListResponse.data.values
 
-    println("Listing all the champions from version $lastestVersion:")
-    championList.data.forEach { (_, champion) ->
-        println(champion.toString())
-    }
+//    println("Listing all the champions from version $lastestVersion:")
+//    championList.data.forEach { (_, champion) ->
+//        println(champion.toString())
+//    }
+
+    // TODO: 14/06 - 1st attempt
+    // filter all Tank/Fighters and select one random
+    val tankFighterChampions = championList.filter { it.roles.contains("Tank") && it.roles.contains("Fighter") }
+    println("Printing every tank/fighter")
+    tankFighterChampions.forEach { println(it) }
+
+    val oneTankFigher = tankFighterChampions.random()
+    println("\n\nThe one who was selected: $oneTankFigher")
 }
